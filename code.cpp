6e47d31d3a4cb81f4dd61c1afd2f936f2977c5f6
omkar31official-stack <omkar31official@gmail.com>
2026-02-08 20:07:02 +0530

    3RD DAY

---
+++ code.cpp
@@ -0,0 +1,150 @@
+// #include<iostream>
+// using namespace std ;
+// int main (){
+
+// int age ;
+// cout<<"enter the age"<<endl;
+// cin>>age;
+// cout<<"the age is "<<age<<endl;
+// return 0; 
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=10, b=20;
+//     cout<<"sum"<<(a+b);
+//     cout<<"difference"<<(a-b);
+//     cout<<"the product"<<(a*b);
+//     return 0;
+
+// // // }
+// // #include<iostream>
+// // using namespace std;
+// // int main (){
+// //     int a,b;
+// //     cout<<"enter the value of a"<<endl;
+// //     cin>>a;
+// //     cout<<"enter the value of b"<<endl;
+// //     cin>>b;
+//     cout<<"the sum is" <<(a+b)<<endl;
+//     return 0;
+// // }
+// // #include <studio
+// #include<iostream>
+// using namespace std;
+
+// // int main(){
+// //     int n;
+
+// //     cout<<"enter the value of n "<<endl;
+// //     cin>>n;
+// //     if(n>=0){
+// //         cout<<"print positive"<<endl;
+// //     }else {
+// //         cout<<"negative"<<endl;
+
+// //     }
+// //     return 0;
+// // }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout<<"enter the age "<<endl;
+//     cin>>age;
+//     if (age>=18){
+//         cout<<"canvote"<<endl;
+//     }else{
+//         cout<<"cant vote"<<endl;
+
+    
+    
+// //     }
+// //     return 0;
+// // }
+// #include<iostream>
+// using namespace std ;
+
+// int main(){
+//     int marks ;
+//     cout <<"enter he marks "<<endl;
+//     cin>>marks;
+//     if(marks>=80 && marks<90){
+//         cout<<"a"<<endl;
+//     }else if (marks>=50 && marks<70){
+//         cout<<"b"<<endl;
+
+//     }else if (marks>=30 && marks<50){
+//         cout<<"c"<<endl;
+
+//     }else{
+//         cout<<"fail"<<endl;
+//     }
+//     return 0;
+
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char ch;
+//     cout<<"enter the charater"<<endl;
+//     cin >>ch;
+//     if(ch>=97 && ch<=122){
+//         cout<<"the leter is smalll letter"<<endl;
+        
+//     }else{
+//         cout<<"the letter is caps";
+
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int count =1;
+//     while (count <=5){
+//         cout<<count<<endl;
+//         count++;
+//     }
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+    
+//     for(int i=1;i<=5;i++){
+//         cout<<i<<endl
+//         ;
+//     }
+    
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n=50;
+//     int oddSum=0;
+//     for(int i=1;
+//     i<=n;
+// i++)
+// if (i % 2!= 0){
+//     oddSum+=1;
+// }
+// cout<<oddSum<<endl;
+// return 0;
+// }
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+     int count;
+     do{
+        count += 1;
+     }while(count<=n);
+    cout<<count<<endl;
+    return 0;
+
+
+}
